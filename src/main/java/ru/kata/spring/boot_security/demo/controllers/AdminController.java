ea9cbcb59cf9d890697ff000c27941e6fc88ae70
Vladalva <vladanovickaa650@gmail.com>
2022-12-01 11:59:14 +0300

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -0,0 +1,64 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final UserService userService;
+
+    public AdminController(UserService userService) {
+        this.userService = userService;
+
+    }
+
+    @GetMapping()
+    public String userList(Model model, Principal principal) {
+        model.addAttribute("user", userService.allUsers());
+        model.addAttribute("admin", userService.findUserByName(principal.getName()));
+        return "/admin";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/create")
+    public String creatUserForm(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("roles", userService.listRoles());
+
+        return "/create";
+    }
+
+    @PostMapping("/create")
+    public String createUser(@ModelAttribute("user") User user) {
+
+        userService.saveUser(user);
+        return "redirect:/admin";
+    }
+
+
+    @GetMapping("/{id}/update")
+    public String updateUser(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("user", userService.findUserById(id));
+        model.addAttribute("roles", userService.listRoles());
+        return "/update";
+
+    }
+
+    @PostMapping("/{id}")
+    public String update(@ModelAttribute("user") User user) {
+        userService.updateUser(user);
+        return "redirect:/admin";
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -0,0 +1,37 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+
+    }
+
+    @GetMapping()
+    public String pageUser(Principal principal, Model model) {
+        model.addAttribute("user", userService.findUserByName(principal.getName()));
+
+        return "/user";
+    }
+
+    @GetMapping("/{id}")
+    public String pageUser(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("roles", userService.listRoles());
+        model.addAttribute("user", userService.findUserById(id));
+        return "/user";
+    }
+}
